9140e359720f056876d0e2faab6ae5021abe3e78
Khyeji <khjji0502@gmail.com>
2024-04-03 21:42:46 +0900

    Project Init

---
+++ Deadzone/Deadzone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deadzone
+//
+//  Created by heyji on 2024/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
