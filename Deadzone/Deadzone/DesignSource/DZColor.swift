1dfb91950aabc14f7e31f16a03958744947c2a71
Khyeji <khjji0502@gmail.com>
2024-04-08 13:55:34 +0900

    Chore: color, font, image asset 추가

--- Deadzone/Deadzone/DesignSource/DZColor.swift
+++ Deadzone/Deadzone/DesignSource/DZColor.swift
@@ -9,10 +9,15 @@ import UIKit
 
 enum DZColor {
     static var black: UIColor = UIColor(named: "ct_black")!
+    static var black02: UIColor = UIColor(named: "ct_subblack")!
+    static var black03: UIColor = UIColor(named: "ct_subblack02")!
     static var grayColor100: UIColor = UIColor(named: "gray100")!
+    static var grayColor154: UIColor = UIColor(named: "gray154")!
     static var subGrayColor100: UIColor = UIColor(named: "subgray100")!
+    static var subGrayColor193: UIColor = UIColor(named: "subgray193")!
     static var grayColorLighting: UIColor = UIColor(named: "graylighting")!
     static var grayColor200: UIColor = UIColor(named: "gray200")!
+    static var grayColor300: UIColor = UIColor(named: "gray300")!
     static var backgroundColor: UIColor = UIColor(named: "ct_bg")!
     static var pointColor: UIColor = UIColor(named: "pointcolor")!
     static var errorColor: UIColor = UIColor(named: "errorcolor")!
